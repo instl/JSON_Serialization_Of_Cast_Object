7b5180c2b8b3ef68cf681e83891931b177bda132
Thomas Harris <tom.harris@just-eat.com>
2014-06-12 09:10:42 +0100

    initial commit

---
+++ JSONSerializationOfCastObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JSONSerializationOfCastObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B InstanceB = new B(){PropA = "A",PropB = "B"};
+
+            A InstanceA = InstanceB;
+
+            var JSONInstanceA = JsonConvert.SerializeObject(InstanceA);
+
+            Console.WriteLine(JSONInstanceA);
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public class A
+    {
+        public string PropA { get; set; }
+    }
+
+      public class B:A
+    {
+        public string PropB { get; set; }
+    }
+}
+
